3bd724865f0f91dae8ab548ea121a2f55734808d
abhik199 <abhishekirar2004@gmail.com>
2026-08-06 11:31:57 +0530

    layout chnage

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
 
-import Layout from "./component/Layout";
+import Layout from "/components/layout";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Blog from "./pages/blog";
